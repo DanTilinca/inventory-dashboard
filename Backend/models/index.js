64c32b4a22c1f2ad0b7d5a0ebc455c4ef38ffca3
DanTpassat <dan.tilinca03@e-uvt.ro>
2024-04-09 21:52:46 +0300

    bd update

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://adminhamza:adminhamza123&@cluster0.pzcviot.mongodb.net/InventoryManagementApp?retryWrites=true&w=majority";
+const uri = "mongodb+srv://dantilinca03:SSl8H5HNOqJFKstH@cluster0.1lnh2xn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 
 function main() {
